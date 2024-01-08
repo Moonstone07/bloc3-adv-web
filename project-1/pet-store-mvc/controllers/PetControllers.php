bac9b168caa3bdf9e6acb7fabe76a9bf2dd73e50
Tischa <84481096+Moonstone07@users.noreply.github.com>
2024-01-07 19:06:20 -0500

    Refactor PetView and PetController classes

--- project-1/pet-store-mvc/controllers/PetControllers.php
+++ project-1/pet-store-mvc/controllers/PetControllers.php
@@ -7,11 +7,9 @@ include_once 'models/model.php';
 
 class PetController
 {
-    private $ToyModel;
 
     public function __construct($conn)
     {
-        $this->ToyModel = new ToyModel($conn);
     }
 
     public function petForm()
@@ -19,32 +17,6 @@ class PetController
         include "views/petForm.php";
     }
 
-    // TOYS
-
-    public function addToyType()
-    {
-        if (isset($_POST['name']) && isset($_POST['price'])) {
-            $toy_name = $_POST['name'];
-            $toy_price = $_POST['price'];
-            $this->ToyModel->insertToy($toy_name, $toy_price);
-        } else {
-            echo "Toy name or price not provided.";
-        }
-    }
-    public function getToys()
-    {
-        return $this->ToyModel->getToys();
-    }
-
-    public function updateToy($id, $new_toy_name, $new_toy_price)
-    {
-        return $this->ToyModel->updateToy($id, $new_toy_name, $new_toy_price);
-    }
-
-    public function deleteToy($id)
-    {
-        return $this->ToyModel->deleteToy($id);
-    }
 
 
 
@@ -52,7 +24,6 @@ class PetController
     // make only one display function then add as you go and call it in the controller
     public function display()
     {
-        $toys = $this->getToys();
         include "views/petView.php";
     }
 }
@@ -74,16 +45,9 @@ $controller->display();
 if (isset($_POST['submit'])) {
     // $controller->addBreedType();
     // $controller->addSpeciesType();
-    $controller->addToyType();
 }
 
-if (isset($_POST['update'])) {
-    $controller->updateToy($_POST['update'], $_POST['new_toy_name'], $_POST['new_toy_price']);
-}
 
-if (isset($_POST['delete'])) {
-    $controller->deleteToy($_POST['delete']);
-}
 
 
 // Calling petForm() only once regardless of whether 'submit' is set
